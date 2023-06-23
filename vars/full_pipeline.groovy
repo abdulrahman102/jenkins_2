dbc986ae0dc264ec8e42809bdd4d684054d3a18b
Abdulrahman <abdoousama1002@gmail.com>
2023-06-23 17:41:48 +0300

    edits3

--- vars/full_pipeline.groovy
+++ vars/full_pipeline.groovy
@@ -1,36 +1,39 @@
-pipeline {
-     agent any
-    parameters {
-        booleanParam(name:'project', defaultValue: true, description:'this paramater help you to know project name')
-        choice(name: 'namespace', choices:['dev','prod','stage'], description: '' ) 
-    }
-
-    stages {
-        stage('check') {
-            steps {
-                echo "checking your code"
-                echo "${params.namespace}"
-               
-            }
+def call(){
+    pipeline {
+         agent any
+        parameters {
+            booleanParam(name:'project', defaultValue: true, description:'this paramater help you to know project name')
+            choice(name: 'namespace', choices:['dev','prod','stage'], description: '' ) 
         }
 
-        stage('test') {
-            when {
-                expression{
-                    params.project == true 
+        stages {
+            stage('check') {
+                steps {
+                    echo "checking your code"
+                    echo "${params.namespace}"
+
                 }
             }
-            steps {
-                echo "testing your app" 
+
+            stage('test') {
+                when {
+                    expression{
+                        params.project == true 
+                    }
+                }
+                steps {
+                    echo "testing your app" 
+                }
             }
-        }
         
-        stage('deployment') {  
-            steps {
-                echo "your code is deployed right now"
-                echo "this build number $BUILD_NUMBER"
-            }
-        }    
+            stage('deployment') {  
+               steps {
+                    echo "your code is deployed right now"
+                    echo "this build number $BUILD_NUMBER"
+                }
+            }    
+        }
+
     }
 
 }
